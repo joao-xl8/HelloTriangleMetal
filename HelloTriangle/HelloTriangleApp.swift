a3b877446376f5981b6266e3c563b28d0e244c5c
joao_arnold.py <joaopaulo167@gmail.com>
2023-09-06 19:29:19 -0300

    Initial Commit

---
+++ HelloTriangle/HelloTriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTriangleApp.swift
+//  HelloTriangle
+//
+//  Created by João Paulo Arnold Barros on 06/09/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloTriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
